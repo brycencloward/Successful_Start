dcf0604aedddc3d4cb917829cdba2b5a89385b23
warrior <warrior@libchkout-mba9.lcsc.edu>
2022-02-14 10:52:52 -0800

    Start page

--- successful-project/src/pages/Start.tsx
+++ successful-project/src/pages/Start.tsx
@@ -1,24 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Start.css';
+import React, { useState } from 'react';
+import './ApplyNow.css';
+import{arrowForward} from "ionicons/icons";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCheckbox, IonList, IonItem, IonLabel, IonItemDivider, IonIcon, IonButton, IonRouterLink, IonButtons } from '@ionic/react';
 
+const checkboxList = [
+  { val: 'I have no substantial work experience-I am just graduating high school.', isChecked: false },
+  { val: 'I have multiple years of experience in a skilled work environment.', isChecked: true },
+  { val: 'I have passed college credit tests (like CLEP) that will count towards my degree.', isChecked: true }
+];
 const Home: React.FC = () => {
+  const [checked, setChecked] = useState(false);
+  
   return (
+    
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+ 
+    <IonContent>
+      <IonList>
+        <IonTitle>What areas do you have experience that might be applied for college credit?</IonTitle>
+
+        {checkboxList.map(({ val, isChecked }, i) => (
+          <IonItem key={i}>
+            <IonLabel>{val}</IonLabel>
+            <IonCheckbox slot="end" value={val} checked={isChecked}  />
+          </IonItem>
+        ))}
+      </IonList>
+          <IonButton>
+          <IonRouterLink routerLink="/pages/ApplyNow">
+          <IonIcon slot="icon-only" icon={arrowForward} />
+          </IonRouterLink>
+        </IonButton>
+    </IonContent>
+  </IonPage>
   );
 };
 
