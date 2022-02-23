819c55808ae52b25f9bd0a8c9735226e00fb4b4b
Brycen Cloward <brycen.cloward@gmail.com>
2022-02-23 11:21:51 -0800

    WorkExpStart changes (work in progress)

--- successful-project/src/pages/WorkExpStart.tsx
+++ successful-project/src/pages/WorkExpStart.tsx
@@ -1,20 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonPage, IonButton, IonLabel, IonIcon, IonItem, IonCheckbox } from '@ionic/react';
+import MainHeader from '../components/MainHeader';
 import './WorkExpStart.css';
+import { arrowForward } from 'ionicons/icons';
 
 const WorkExpStart: React.FC = () => {
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(true);
+
+  //function that deteccts if checkbox one was checked, and then unchecks the other box
+  const changeOptionOne = () => {
+    console.log("First item checked!");
+    setChecked2(false);
+    
+    //if(option2.isChecked=true)
+  }
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <MainHeader />
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonItem>
+          <IonCheckbox
+            checked={checked1}
+            name="option1"
+            onIonChange={changeOptionOne}
+          />
+          <IonLabel class="ion-text-wrap">I have no substantial work experience - I'm just graduating high school</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonCheckbox checked={checked2} name="option2" onIonChange={e => console.log("Second item checked!")} />
+          <IonLabel class="ion-text-wrap">I have multiple years of experience in a skilled work environment</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonCheckbox checked={false} name="option3" onIonChange={e => console.log("Third item checked!")} />
+          <IonLabel class="ion-text-wrap">I have passed college credit tests (like CLEP) that will count towards my degree.</IonLabel>
+        </IonItem>
+
+        <IonButton color="tertiary" expand="block" routerLink="WorkExpCreditResults">
+          <IonLabel>Next  <IonIcon icon={arrowForward} /></IonLabel>
+        </IonButton>
+
       </IonContent>
     </IonPage>
   );
