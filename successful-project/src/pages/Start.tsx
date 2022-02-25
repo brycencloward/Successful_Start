92f292a3b784d4ab3261f98fc3c65c4bcbc0d16c
warrior <warrior@libchkout-mba9.lcsc.edu>
2022-02-25 11:42:27 -0800

    start page

--- successful-project/src/pages/Start.tsx
+++ successful-project/src/pages/Start.tsx
@@ -4,13 +4,13 @@ import{arrowForward} from "ionicons/icons";
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCheckbox, IonList, IonItem, IonLabel, IonItemDivider, IonIcon, IonButton, IonRouterLink, IonButtons } from '@ionic/react';
 import MainHeader from '../components/MainHeader';
 
+  
 const checkboxList = [
   { val: 'I have no substantial work experience-I am just graduating high school.', isChecked: false },
   { val: 'I have multiple years of experience in a skilled work environment.', isChecked: true },
   { val: 'I have passed college credit tests (like CLEP) that will count towards my degree.', isChecked: true }
 ];
 const Home: React.FC = () => {
-  const [checked, setChecked] = useState(false);
   
   return (
     
@@ -27,10 +27,11 @@ const Home: React.FC = () => {
           </IonItem>
         ))}
       </IonList>
-          <IonButton color="tertiary" expand="block">
+          <IonButton color="tertiary" expand="block" routerLink='ApplyNow'>
+            
           <IonLabel>Next</IonLabel>
           <IonIcon icon={arrowForward} />
-        </IonButton>
+       </IonButton>
     </IonContent>
   </IonPage>
   );
