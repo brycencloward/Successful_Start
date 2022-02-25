109e0247dc3b55a871a050e50928048f01758a0c
AzizBouba <maboubaker@lcmail.lcsc.edu>
2022-02-25 11:23:40 -0800

    WhichCLEP and Which Tests pages plus link between them

--- successful-project/src/pages/WhichCLEP.tsx
+++ successful-project/src/pages/WhichCLEP.tsx
@@ -1,14 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { arrowForward } from 'ionicons/icons';
 import MainHeader from '../components/MainHeader';
 import './WhichCLEP.css';
+
+const checkboxList = [
+  { val: 'Business', isChecked: true },
+  { val: 'Composition & Literature', isChecked: false },
+  { val: 'World Languages', isChecked: false },
+  { val: 'Science & Mathematics', isChecked: false },
+  { val: 'History & Social Sciences', isChecked: false }
+];
 
 const WhichCLEP: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+  <MainHeader/>
+      <IonHeader class= "ion-text-center">
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonLabel className= "ion-text-wrap"> Please select the subjects in which you have completed CLEP examinations</IonLabel>
+          {checkboxList.map(({ val, isChecked }, i) => (
+          <IonItem key={i}>
+            <IonLabel>{val}</IonLabel>
+            <IonCheckbox slot="end" value={val} checked={isChecked} color="tertiary"/>
+          </IonItem>
+        ))}
         </IonToolbar>
+        <IonButton color="tertiary" expand="block">
+          <IonLabel>Next</IonLabel>
+          <IonIcon icon={arrowForward} />
+        </IonButton>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
--- successful-project/src/pages/WhichTests.tsx
+++ successful-project/src/pages/WhichTests.tsx
@@ -1,13 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { arrowForward } from 'ionicons/icons';
+import MainHeader from '../components/MainHeader';
 import './WhichTests.css';
+
+const checkboxList = [
+  { val: 'AP Exam', isChecked: false },
+  { val: 'CLEP Exam', isChecked: true },
+  { val: 'Dantes/DSST', isChecked: false },
+  { val: 'IB Exam(s)', isChecked: false }
+];
 
 const WhichTests: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+  <MainHeader/>
+      <IonHeader class= "ion-text-center">
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonLabel className= "ion-text-wrap"> Please select the type of test(s) you have completed and passed</IonLabel>
+          {checkboxList.map(({ val, isChecked }, i) => (
+          <IonItem key={i}>
+            <IonLabel>{val}</IonLabel>
+            <IonCheckbox slot="end" value={val} checked={isChecked} color="tertiary"/>
+          </IonItem>
+        ))}
         </IonToolbar>
+        <IonButton color="tertiary" expand="block" routerLink='WhichCLEP'>
+          <IonLabel>Next</IonLabel>
+          <IonIcon icon={arrowForward} />
+        </IonButton>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
