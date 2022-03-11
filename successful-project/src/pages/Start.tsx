bc7447094615ce6d6482a96b77d69562c2c81e24
warrior <warrior@libchkout-mba9.lcsc.edu>
2022-03-11 11:21:56 -0800

    in app browser

--- successful-project/src/pages/Start.tsx
+++ successful-project/src/pages/Start.tsx
@@ -3,13 +3,16 @@ import './Start.css';
 import{arrowForward} from "ionicons/icons";
 import { IonContent, IonPage, IonTitle , IonCheckbox, IonList, IonItem, IonLabel, IonIcon, IonButton } from '@ionic/react';
 import MainHeader from '../components/MainHeader';
-
+import { Browser } from '@capacitor/browser';
   
 const checkboxList = [
   { val: 'I have no substantial work experience-I am just graduating high school.', isChecked: false },
   { val: 'I have multiple years of experience in a skilled work environment.', isChecked: false },
   { val: 'I have passed college credit tests (like CLEP) that will count towards my degree.', isChecked: true }
 ];
+const openCapacitorSite = async () => {
+  await Browser.open({ url: 'https://www.lcsc.edu/admissions/apply' });
+};
 const Home: React.FC = () => {
   
   return (
@@ -27,7 +30,7 @@ const Home: React.FC = () => {
           </IonItem>
         ))}
       </IonList>
-          <IonButton color="tertiary" expand="block" routerLink='WhichTests'>
+          <IonButton color="tertiary" expand="block" onClick={openCapacitorSite}>
             
           <IonLabel>Next</IonLabel>
           <IonIcon icon={arrowForward} />
