c2ebfe9a20f13e442dd892b4519da78ce57c120e
AzizBouba <maboubaker@lcmail.lcsc.edu>
2022-03-18 12:52:44 -0700

    WhichDSST complete

--- successful-project/src/pages/WhichDSST.tsx
+++ successful-project/src/pages/WhichDSST.tsx
@@ -17,73 +17,66 @@ const WhichDSST: React.FC = () => {
           <IonLabel className="ion-text-wrap">Please select all the subjects in which you have completed DSST examinations</IonLabel>
 
           <IonItem class="ion-text-center" lines="none">
+            <IonLabel>Arts & Comm</IonLabel>
+            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
+              <IonSelectOption value="CoreDA" class="test">Art of the Western World</IonSelectOption>
+              <IonSelectOption value="COMM204" class="test">Principles of Public Speaking</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem class="ion-text-center" lines="none">
+            <IonLabel>English</IonLabel>
+            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
+              <IonSelectOption value="ENGL102">Principles of Advance English Composition</IonSelectOption>
+              <IonSelectOption value="ENGL203">Technical Writing</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem class="ion-text-center" lines="none">
+            <IonLabel>Humanities</IonLabel>
+            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
+              <IonSelectOption value="ID300D">Environmental Science (Environment and Humanity)</IonSelectOption>
+              <IonSelectOption value="CoreINS">Ethics in America</IonSelectOption>
+              <IonSelectOption value="ID300E">Ethics in Technology</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem class="ion-text-center" lines="none">
+            <IonLabel>Technology</IonLabel>
+            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
+              <IonSelectOption value="LowElec">Computing & Information Technology</IonSelectOption>
+              <IonSelectOption value="ID300E">Ethics in Technology</IonSelectOption>
+              <IonSelectOption value="UpElec">Fundamentals of Cybersecurity</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem class="ion-text-center" lines="none">
             <IonLabel>Business</IonLabel>
             <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
-              <IonSelectOption value="FinAcc" class="test">Financial Accounting</IonSelectOption>
-              <IonSelectOption value="InfoSys" class="test">Information Systems</IonSelectOption>
-              <IonSelectOption value="IntroBusLaw">Introductory Business Law</IonSelectOption>
-              <IonSelectOption value="PrincManagement">Principles of Management</IonSelectOption>
-              <IonSelectOption value="PrincMarketing">Principles of Marketing</IonSelectOption>
+              <IonSelectOption value="GNBT270">Business Ethics & Society</IonSelectOption>
+              <IonSelectOption value="BUS101">Introduction to Business</IonSelectOption>
+              <IonSelectOption value="BUS355">Management Information Systems</IonSelectOption>
+              <IonSelectOption value="GNBT202">Personal Finance</IonSelectOption>
             </IonSelect>
           </IonItem>
 
           <IonItem class="ion-text-center" lines="none">
-            <IonLabel>Composition & Literature</IonLabel>
+            <IonLabel>Math</IonLabel>
             <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
-              <IonSelectOption value="AmericanLit">American Literature</IonSelectOption>
-              <IonSelectOption value="AILit">Analyzing and Interpreting Literature</IonSelectOption>
-              <IonSelectOption value="CollegeComp">College Composition</IonSelectOption>
-              <IonSelectOption value="EnglishLit">English Literature</IonSelectOption>
-              <IonSelectOption value="FCollegeComp">Freshman College Composition</IonSelectOption>
+              <IonSelectOption value="GNBT202">Business Mathematics</IonSelectOption>
+              <IonSelectOption value="MTHPT153">Principles of Statistics</IonSelectOption>
             </IonSelect>
           </IonItem>
 
           <IonItem class="ion-text-center" lines="none">
-            <IonLabel>World Languages</IonLabel>
+            <IonLabel>Social Science</IonLabel>
             <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
-              <IonSelectOption value="SpanishI">Spanish Language - Level I</IonSelectOption>
-              <IonSelectOption value="SpanishII">Spanish Language - Level II</IonSelectOption>
-              <IonSelectOption value="SpanishWritI">Spanish with Writing - Level I</IonSelectOption>
-              <IonSelectOption value="SpanishWritII">Spanish with Writing - Level II</IonSelectOption>
-              <IonSelectOption value="FrenchI">French Language - Level I</IonSelectOption>
-              <IonSelectOption value="FrenchII">French Language - Level II</IonSelectOption>
-              <IonSelectOption value="GermanI">German Language - Level I</IonSelectOption>
-              <IonSelectOption value="GermanII">German Language - Level II</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-
-          <IonItem class="ion-text-center" lines="none">
-            <IonLabel>Science & Mathematics</IonLabel>
-            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
-              <IonSelectOption value="Bio">Biology</IonSelectOption>
-              <IonSelectOption value="Calc">Calculus</IonSelectOption>
-              <IonSelectOption value="Chem">Chemistry</IonSelectOption>
-              <IonSelectOption value="Alg">College Algebra</IonSelectOption>
-              <IonSelectOption value="Math">College Mathematics</IonSelectOption>
-              <IonSelectOption value="NatSci">Natural Sciences General Exam</IonSelectOption>
-              <IonSelectOption value="PreCalc">Pre Calculus</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-
-          <IonItem class="ion-text-center" lines="none">
-            <IonLabel>History & Social Sciences</IonLabel>
-            <IonSelect interface="popover" interfaceOptions={options} multiple={true}>
-              <IonSelectOption value="AmerGov">American Government</IonSelectOption>
-              <IonSelectOption value="HisUSI">History of the United States I: Early Colonization to 1877</IonSelectOption>
-              <IonSelectOption value="HisUSII">History of the United States II: 1865 to the Present</IonSelectOption>
-              <IonSelectOption value="HumGrowthDev">Human Growth & Development</IonSelectOption>
-              <IonSelectOption value="IntroEdPsychology">Intro to Educational Psychology</IonSelectOption>
-              <IonSelectOption value="IntroPsychology">Intro to Psychology</IonSelectOption>
-              <IonSelectOption value="IntroSociology">Intro to Sociology</IonSelectOption>
-              <IonSelectOption value="MacroEcon">Macroeconomics</IonSelectOption>
-              <IonSelectOption value="MicroEcon">Microeconomics</IonSelectOption>
-              <IonSelectOption value="WestCivI">Western Civilization I: Ancient Near East to 1648</IonSelectOption>
-              <IonSelectOption value="WestCivII">Western Civilivation II: 1648 to the Present</IonSelectOption>
+              <IonSelectOption value="HRPT185">Organizational Behavior</IonSelectOption>
             </IonSelect>
           </IonItem>
 
         </IonToolbar>
-        <IonButton color="tertiary" expand="block" routerLink='WhichBusinessCLEP'>
+        <IonButton color="tertiary" expand="block" routerLink='CreditResults'>
           <IonLabel>Next</IonLabel>
           <IonIcon icon={arrowForward} />
         </IonButton>
