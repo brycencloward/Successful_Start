c64cd78ddf713c2e6f374bfdc3b3ab562a50e246
AzizBouba <maboubaker@lcmail.lcsc.edu>
2022-03-17 21:28:54 -0700

    fixed start page routing and added proto routing

--- successful-project/src/pages/WhichTests.tsx
+++ successful-project/src/pages/WhichTests.tsx
@@ -1,16 +1,56 @@
-import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
 import { arrowForward } from 'ionicons/icons';
+import { useState } from 'react';
 import MainHeader from '../components/MainHeader';
 import './WhichTests.css';
 
-const checkboxList = [
-  { val: 'AP Exam', isChecked: false },
-  { val: 'CLEP Exam', isChecked: true },
-  { val: 'Dantes/DSST', isChecked: false },
-  { val: 'IB Exam(s)', isChecked: false }
-];
-
 const WhichTests: React.FC = () => {
+
+  const router = useIonRouter(); //needed to route programmatically
+
+  const [AP, APexam] = useState(false);
+  const [CLEP, CLEPexam] = useState(false);
+  const [DSST, DantesDSST] = useState(false);
+  const [IB, IBExam] = useState(false);
+
+  const checkboxList = [
+    { val: "AP Exam", isChecked: false },
+    { val: "CLEP Exam", isChecked: true },
+    { val: "Dantes/DSST", isChecked: false },
+    { val: "IB Exam(s)", isChecked: false }
+  ];
+
+  const checkBoxChange = (event: any) => {
+    if(event.target.value == "AP Exam"){
+      APexam(event.target.checked);
+    }
+    else if(event.target.value == "CLEP Exam"){
+      CLEPexam(event.target.checked);
+    }
+    else if(event.target.value == "Dantes/DSST"){
+      DantesDSST(event.target.checked);
+    }
+    else if(event.target.value == "IB Exam(s)"){
+      IBExam(event.target.checked);
+    }
+  }
+  const experienceCheck = (event: any) => {
+    //console.log("This is working!");
+    //console.log(noExp, workExp, testsPassed);
+    if(AP){
+      router.push("WhichCLEP", "forward", "push");
+    }
+    else if(CLEP) {
+      router.push("WhichCLEP", "forward", "push");
+    }
+    else if(DSST){
+      router.push("WhichCLEP", "forward", "push");
+    }
+    else if(IB){
+      router.push("WhichCLEP", "forward", "push");
+    }
+  };
+
   return (
     <IonPage>
   <MainHeader/>
@@ -20,11 +60,11 @@ const WhichTests: React.FC = () => {
           {checkboxList.map(({ val, isChecked }, i) => (
           <IonItem class="ion-text-center" lines="none" key={i}>
             <IonLabel>{val}</IonLabel>
-            <IonCheckbox slot="end" value={val} checked={isChecked} color="tertiary"/>
+            <IonCheckbox slot="end" value={val} color="tertiary" onIonChange={(e) => checkBoxChange(e)}/>
           </IonItem>
         ))}
         </IonToolbar>
-        <IonButton color="tertiary" expand="block" routerLink='WhichCLEP'>
+        <IonButton color="tertiary" expand="block" onClick={(e) => experienceCheck(e)}>
           <IonLabel>Next</IonLabel>
           <IonIcon icon={arrowForward} />
         </IonButton>
